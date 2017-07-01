aa4b12a524e6d1079422f686efdf7449270bab9c
damianedwards <damian@damianedwards.com>
2017-06-30 17:08:42 -0700

    Add initial MarkdownTagHelper - #6 - Honor VersionSuffix in build

---
+++ src/TagHelperPack/MarkdownTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagHelperPack
+{
+    [HtmlTargetElement("markdown")]
+    public class MarkdownTagHelper : TagHelper
+    {
+        // TODO: Support a 'src' attribute for specifying a source markdown file
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var markdown = await output.GetChildContentAsync();
+            var html = Markdig.Markdown.ToHtml(markdown.GetContent(NullHtmlEncoder.Default));
+            output.Content.SetHtmlContent(html);
+            output.TagName = null;
+        }
+    }
+}
