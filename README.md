7b7eda5e30fb4a3e909a6eb0cd4444d7aaf846ce
damianedwards <damian@damianedwards.com>
2017-06-30 17:36:35 -0700

    Add caching to MarkdownTagHelper: - #6 - Add MyGet shield to README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# TagHelperPack [![Build status](https://ci.appveyor.com/api/projects/status/6943lgsvtg2c8jcu/branch/master?svg=true)](https://ci.appveyor.com/project/DamianEdwards/taghelperpack/branch/master)
+# TagHelperPack [![Build status](https://ci.appveyor.com/api/projects/status/6943lgsvtg2c8jcu/branch/master?svg=true)](https://ci.appveyor.com/project/DamianEdwards/taghelperpack/branch/master) [![MyGet Pre Release](https://img.shields.io/myget/taghelperpack-ci/vpre/TagHelperPack.svg)]()
 
 A set of useful, and possibly opinionated, Tag Helpers for ASP.NET Core
--- src/TagHelperPack/MarkdownTagHelper.cs
+++ src/TagHelperPack/MarkdownTagHelper.cs
@@ -1,20 +1,41 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
 
 namespace TagHelperPack
 {
+    /// <summary>
+    /// Renders markdown.
+    /// </summary>
     [HtmlTargetElement("markdown")]
     public class MarkdownTagHelper : TagHelper
     {
+        private static readonly IMemoryCache Cache;
+
+        static MarkdownTagHelper()
+        {
+            var cacheOptions = new MemoryCacheOptions
+            {
+                CompactOnMemoryPressure = false
+            };
+            Cache = new MemoryCache(new OptionsWrapper<MemoryCacheOptions>(cacheOptions));
+        }
+
         // TODO: Support a 'src' attribute for specifying a source markdown file
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var markdown = await output.GetChildContentAsync();
-            var html = Markdig.Markdown.ToHtml(markdown.GetContent(NullHtmlEncoder.Default));
+            var cacheKey = context.UniqueId;
+            var html = await Cache.GetOrCreateAsync(cacheKey, async entry =>
+            {
+                var markdown = await output.GetChildContentAsync();
+                return Markdig.Markdown.ToHtml(markdown.GetContent(NullHtmlEncoder.Default));
+            });
+
             output.Content.SetHtmlContent(html);
             output.TagName = null;
         }
