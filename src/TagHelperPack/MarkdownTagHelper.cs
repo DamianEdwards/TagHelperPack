187e4a13ca94f30f49183b9463e6082fe96dabeb
Phil Haack <haacked@gmail.com>
2022-07-10 10:23:46 -0700

    Implement Html support for MarkdownTagHelper

--- src/TagHelperPack/MarkdownTagHelper.cs
+++ src/TagHelperPack/MarkdownTagHelper.cs
@@ -1,9 +1,19 @@
-﻿using System.IO;
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Ganss.XSS;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.AspNetCore.Html;
 using Markdig;
+using Markdig.Extensions.Abbreviations;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Syntax;
 
 namespace TagHelperPack
 {
@@ -15,30 +25,129 @@ namespace TagHelperPack
     {
         // TODO: Support a 'src' attribute for specifying a source markdown file
 
-        /// <inheritdoc />
+        /// <summary>
+        /// When <c>true</c>, allows sanitized HTML to be rendered.
+        /// </summary>
+        /// <remarks>
+        /// Makes use of
+        /// </remarks>
+        [HtmlAttributeName("allow-html")]
+        public bool AllowHtml { get; set; }
+
+        /// <summary>
+        /// When set to true (default) strips leading white space based on the first line of non-empty content. The
+        /// first line of content determines the format of the white spacing and removes q`it from all other lines.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// When pulling markdown from a variable, to get full fidelity, you may want to do this:
+        /// &lt;markdown normalize-whitespace="false"&gt;@yourMarkdown&lt;/markdown&gt; otherwise cases where the markdown
+        /// you're rendering starts with an indented code block will not render correctly.
+        /// </para>
+        /// <para>
+        /// This approach is adapted from
+        /// <see href="https://github.com/RickStrahl/Westwind.AspNetCore.Markdown">Westwind.AspNetCore.Markdown</see>
+        /// (MIT license).
+        /// </para>
+        /// </remarks>
+        [HtmlAttributeName("normalize-whitespace")]
+        public bool NormalizeWhitespace { get; set; } = true;
+
+>>>>>>> 0ba9182 (Implement Html support for MarkdownTagHelper)
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            var markdownRazorContent = await output.GetChildContentAsync(NullHtmlEncoder.Default);
-            var markdownHtmlContent = new MarkdownHtmlContent(markdownRazorContent.GetContent(NullHtmlEncoder.Default));
+            var childContent = await output.GetChildContentAsync(NullHtmlEncoder.Default);
+            var razorContent = childContent.GetContent(NullHtmlEncoder.Default);
+            if (razorContent is not { Length: > 0 })
+            {
+                return;
+            }
+            var markdown = NormalizeWhitespace ? NormalizeWhiteSpaceText(razorContent) : razorContent;
 
-            output.Content.SetHtmlContent(markdownHtmlContent);
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+            if (!AllowHtml)
+            {
+                pipelineBuilder = pipelineBuilder.DisableHtml();
+            }
+            var pipeline = pipelineBuilder.Build();
+            var html = Markdown.ToHtml(markdown, pipeline);
+            var sanitizedHtml = SanitizeHtml(html);
+
+            output.Content.SetHtmlContent(sanitizedHtml);
             output.TagName = null;
+            await base.ProcessAsync(context, output);
         }
 
-        private class MarkdownHtmlContent : IHtmlContent
+        /// <summary>
+        /// Strips leading white space based on shortest indented non-empty line.
+        /// </summary>
+        /// <remarks>
+        /// The reason this method exists is indented markdown is considered to be preformatted code blocks. And the
+        /// the markdown this tag helper receives might all be indented because of where the markdown tag helper is used
+        /// within the page (such as indented within a div).
+        /// </remarks>
+        static string NormalizeWhiteSpaceText(string text)
         {
-            private readonly string _markdown;
-
-            public MarkdownHtmlContent(string markdown)
+            if (text is { Length: 0 })
             {
-                _markdown = markdown;
+                return text;
             }
 
-            public void WriteTo(TextWriter writer, HtmlEncoder encoder)
+            var lineReader = new StringReader(text);
+            // In the case of something like this, the first line will be empty, so we need to advance to the first
+            // non-empty line.
+            // <markdown>
+            //    @someMarkdown
+            // </markdown>
+            var firstLine = lineReader.ReadLine();
+            while (firstLine is not { Length: > 0 })
             {
-                var pipeline = new MarkdownPipelineBuilder().DisableHtml().Build();
-                Markdig.Markdown.ToHtml(_markdown, writer, pipeline);
+                firstLine = lineReader.ReadLine();
             }
+
+            if (firstLine is not { Length: > 0 })
+            {
+                return text;
+            }
+
+            var indent = GetIndentation(firstLine); // We're going to use this to strip leading white space.
+
+            return string.Join("\n", GetLines(lineReader, firstLine, indent));
+        }
+
+        static IEnumerable<string> GetLines(TextReader stringReader, string firstLine, int indent)
+        {
+            yield return firstLine.Substring(indent);
+
+            while (stringReader.ReadLine() is { } line)
+            {
+                var indentation = GetIndentation(line);
+                yield return indentation < indent
+                    ? line
+                    : line.Substring(indent);
+            }
+        }
+
+        static int GetIndentation(string line)
+        {
+            int num = 0;
+            while (num < line.Length && char.IsWhiteSpace(line[num]))
+                ++num;
+            return num;
+        }
+
+        static string SanitizeHtml(string markdownContent)
+        {
+            var sanitizer = new HtmlSanitizer();
+            sanitizer.AllowedTags.Add("blockquote");
+            sanitizer.AllowedTags.Add("footer");
+            sanitizer.AllowedTags.Add("video");
+            sanitizer.AllowedTags.Add("source");
+            sanitizer.AllowedTags.Add("iframe");
+            sanitizer.AllowedAttributes.Add("class");
+            sanitizer.AllowedAttributes.Add("controls");
+
+            return sanitizer.Sanitize(markdownContent);
         }
     }
 }
---
+++ tests/UnitTests/MarkdownTagHelperTests.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TagHelperPack;
+using Xunit;
+
+public class MarkdownTagHelperTests
+{
+    public class TheProcessAsyncMethod
+    {
+        [Fact]
+        public async Task RendersHtmlFromMarkdown()
+        {
+            var tagHelper = new MarkdownTagHelper();
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+            var tagHelperOutput = new TagHelperOutput(
+                "markdown",
+                new TagHelperAttributeList(),
+                (_, _) => {
+                    var tagHelperContent = new DefaultTagHelperContent()
+                        .SetHtmlContent(new HtmlString("*Italic*, **bold**, and `monospace`."));
+                    return Task.FromResult(tagHelperContent);
+                });
+            await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
+
+            Assert.Null(tagHelperOutput.TagName);
+            Assert.Equal("<p><em>Italic</em>, <strong>bold</strong>, and <code>monospace</code>.</p>\n",
+                tagHelperOutput.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task RendersMarkdownWithHtmlEncodedByDefault()
+        {
+            var tagHelper = new MarkdownTagHelper { NormalizeWhitespace = true };
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+            var tagHelperOutput = new TagHelperOutput(
+                "markdown",
+                new TagHelperAttributeList(),
+                (_, _) => {
+                    var tagHelperContent = new DefaultTagHelperContent()
+                        .SetHtmlContent(new HtmlString(@"
+    # Title one
+
+    Before image
+
+    <img src onerror=alert(document.cookie)>
+
+    After image
+
+    ## Subject two
+
+    Test"));
+                    return Task.FromResult(tagHelperContent);
+                });
+            await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
+
+            Assert.Null(tagHelperOutput.TagName);
+            Assert.Equal(@"<h1>Title one</h1>
+<p>Before image</p>
+<p>&lt;img src onerror=alert(document.cookie)&gt;</p>
+<p>After image</p>
+<h2>Subject two</h2>
+<p>Test</p>
+",
+                tagHelperOutput.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task RendersSanitizedHtmlWhenAllowHtmlTrue()
+        {
+            var tagHelper = new MarkdownTagHelper { AllowHtml = true, NormalizeWhitespace = true };
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+            var tagHelperOutput = new TagHelperOutput(
+                "markdown",
+                new TagHelperAttributeList(),
+                (_, _) => {
+                    var tagHelperContent = new DefaultTagHelperContent()
+                        .SetHtmlContent(new HtmlString(@"
+    # Title one
+
+    Before image
+
+    <img src onerror=alert(document.cookie)>
+
+    After image
+
+    ## Subject two
+
+    Test"));
+                    return Task.FromResult(tagHelperContent);
+                });
+            await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
+
+            Assert.Null(tagHelperOutput.TagName);
+            Assert.Equal(@"<h1>Title one</h1>
+<p>Before image</p>
+<img src="""">
+<p>After image</p>
+<h2>Subject two</h2>
+<p>Test</p>
+",
+                tagHelperOutput.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task RendersMarkdownThatStartsWithIndentedCodeBlock()
+        {
+            var tagHelper = new MarkdownTagHelper { NormalizeWhitespace = false };
+            var tagHelperContext = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(),
+                Guid.NewGuid().ToString("N"));
+            var tagHelperOutput = new TagHelperOutput(
+                "markdown",
+                new TagHelperAttributeList(),
+                (_, _) => {
+                    var tagHelperContent = new DefaultTagHelperContent()
+                        .SetHtmlContent(new HtmlString(@"
+    Some Code
+
+Not Code
+"));
+                    return Task.FromResult(tagHelperContent);
+                });
+            await tagHelper.ProcessAsync(tagHelperContext, tagHelperOutput);
+
+            Assert.Null(tagHelperOutput.TagName);
+            Assert.Equal(@"<pre><code>Some Code
+</code></pre>
+<p>Not Code</p>
+",
+                tagHelperOutput.Content.GetContent());
+        }
+    }
+}
