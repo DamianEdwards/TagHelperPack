9235e3de631af82cdf35c2f1a748569764082933
Ricky Tobing <bingzer@users.noreply.github.com>
2023-09-24 16:37:02 -0400

    Add for [asp-enabled]="boolean" for button, input, etc... (#57)
    * EnabledTagHelper initial check-in
    * EnabledTagHelper unit tests
    * Update src/TagHelperPack/EnabledTagHelper.cs
    Co-authored-by: Damian Edwards <damian@damianedwards.com>
    * Update src/TagHelperPack/EnabledTagHelper.cs
    Co-authored-by: Damian Edwards <damian@damianedwards.com>
    * Use [Theory] to test
    * Update comments
    ---------
    Co-authored-by: Damian Edwards <damian@damianedwards.com>

---
+++ src/TagHelperPack/EnabledTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelperPack;
+
+/// <summary>
+/// Enable/disable elements. Disabling by adding <c>disabled="disabled"</c> attribute to the element.
+/// Supported elements: https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled
+/// </summary>
+[HtmlTargetElement("button", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("fieldset", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("keygen", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("optgroup", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("option", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("select", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("textarea", Attributes = "[asp-enabled]")]
+[HtmlTargetElement("input", Attributes = "[asp-enabled]")]
+public class EnabledTagHelper : TagHelper
+{
+    /// <summary>
+    /// Enable this element when the condition is <c>true</c>. Defaults to <c>true</c>.
+    /// If <c>false</c>, will add <c>disabled="disabled"</c> attribute to the element.
+    /// </summary>
+    [HtmlAttributeName("asp-enabled")]
+    public bool IsEnabled { get; set; } = true;
+
+    /// <inheritdoc />
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        if (context.SuppressedByAspIf())
+        {
+            return;
+        }
+
+        // If it's not enabled, append disabled attribute
+        if (!IsEnabled)
+        {
+            output.Attributes.SetAttribute("disabled", "disabled");
+        }
+    }
+}
---
+++ tests/UnitTests/EnabledTagHelperTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagHelperPack;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EnabledTagHelperTests
+    {
+        [Fact]
+        public void IsIntanceOf_TagHelper()
+        {
+            var helper = new EnabledTagHelper();
+
+            Assert.IsAssignableFrom<TagHelper>(helper);
+        }
+
+        [Fact]
+        public void IsEnabled_True_ByDefault()
+        {
+            var helper = new EnabledTagHelper();
+
+            Assert.True(helper.IsEnabled);
+        }
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void Process(bool isTagEnabled, bool containsDisabledAttribute)
+        {
+            var list = new TagHelperAttributeList();
+
+            var context = new TagHelperContext("button", list, new Dictionary<object, object>(), "unqiueId");
+
+            var output = new TagHelperOutput("button", list, (_, _) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+            output.TagName = "button";
+            output.Content.SetHtmlContent("hello");
+
+            var helper = new EnabledTagHelper();
+            helper.IsEnabled = isTagEnabled;
+
+            helper.Process(context, output);
+
+            Assert.Equal("button", output.TagName);
+            Assert.Equal(containsDisabledAttribute, output.Attributes.ContainsName("disabled"));
+        }
+
+    }
+}
